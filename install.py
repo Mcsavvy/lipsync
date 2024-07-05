91d4befd8c98657a50223ca1726568e0940d14ae
John David <davemcsavvii@gmail.com>
2024-07-05 09:59:49 +0000

    feat: Smoothen project reinstallation

--- install.py
+++ install.py
@@ -1,42 +1,49 @@
-version = 'v8.3'
-
 import os
 import shutil
 import subprocess
 
 from easy_functions import load_file_from_url, load_model, load_predictor
+from utils import dump_database, load_database
+import warnings
+from os import path
 
 # Get the location of the basicsr package
-import warnings
 
-warnings.filterwarnings("ignore", category=UserWarning, module="torchvision.transforms.functional_tensor")
+version = "v8.3"
+
+warnings.filterwarnings(
+    "ignore", category=UserWarning, module="torchvision.transforms.functional_tensor"
+)
+
 
 # Get the location of the basicsr package
 def get_basicsr_location():
-    result = subprocess.run(['pip', 'show', 'basicsr'], capture_output=True, text=True)
-    for line in result.stdout.split('\n'):
-        if 'Location: ' in line:
-            loc = line.split('Location: ')[1]
+    result = subprocess.run(["pip", "show", "basicsr"], capture_output=True, text=True)
+    for line in result.stdout.split("\n"):
+        if "Location: " in line:
+            loc = line.split("Location: ")[1]
             if loc.endswith("site-packages") or loc.endswith("dist-packages"):
-                loc = os.path.join(loc, 'basicsr')
-            if loc.endswith('basicsr'):
-                loc = os.path.join(loc, 'data')
+                loc = path.join(loc, "basicsr")
+            if loc.endswith("basicsr"):
+                loc = path.join(loc, "data")
             return loc
     return None
+
 
 # Move and replace a file to the basicsr location
 def move_and_replace_file_to_basicsr(file_name):
     basicsr_location = get_basicsr_location()
     if basicsr_location:
-        destination = os.path.join(basicsr_location, file_name)
+        destination = path.join(basicsr_location, file_name)
         # Move and replace the file
         shutil.copyfile(file_name, destination)
-        print(f'File replaced at {destination}')
+        print(f"File replaced at {destination}")
     else:
-        print('Could not find basicsr location.')
+        print("Could not find basicsr location.")
+
 
 # Example usage
-file_to_replace = 'degradations.py'  # Replace with your file name
+file_to_replace = "degradations.py"  # Replace with your file name
 move_and_replace_file_to_basicsr(file_to_replace)
 
 
@@ -44,65 +51,152 @@ from enhance import load_sr
 
 working_directory = os.getcwd()
 
+
+if path.exists("installed.txt"):
+    print("Already installed!")
+    print("If you want to reinstall, delete the installed.txt file")
+    exit(0)
+
+has_previous_installation = input(
+    "Have you previously installed this app? (y/n): "
+).strip().lower() in ["y", "yes"]
+
+if has_previous_installation:
+    installation_path: str | None = None
+    while installation_path is None:
+        try:
+            installation_path = input(
+                "Enter the path where you installed the app: "
+            ).strip()
+        except (KeyboardInterrupt, EOFError):
+            print("Exiting...")
+            exit(0)
+        if not path.exists(installation_path):
+            print("Invalid path. Try again.")
+            installation_path = None
+        if not path.isdir(installation_path):
+            print("Path is not a directory. Try again.")
+            installation_path = None
+        if not path.exists(path.join(installation_path, "invoke.py")):
+            print("Path does not contain the app. Try again.")
+            installation_path = None
+        installation_path = path.abspath(installation_path)
+    if path.exists(path.join(installation_path, "checkpoints")):
+        print(f"Copying models from {installation_path}")
+        shutil.copytree(
+            path.join(installation_path, "checkpoints"),
+            path.join(working_directory, "checkpoints"),
+            dirs_exist_ok=True,
+        )
+    if path.exists(path.join(installation_path, "face_alignment")):
+        print(f"Copying face alignment from {installation_path}")
+        shutil.copytree(
+            path.join(installation_path, "face_alignment"),
+            path.join(working_directory, "face_alignment"),
+            dirs_exist_ok=True,
+        )
+    if path.exists(path.join(installation_path, "result")):
+        print(f"Copying synced videos from {installation_path}")
+        shutil.copytree(
+            path.join(installation_path, "result"),
+            path.join(working_directory, "result"),
+            dirs_exist_ok=True,
+        )
+    if path.exists(path.join(installation_path, "gfpgan")):
+        print(f"Copying gfpgan from {installation_path}")
+        shutil.copytree(
+            path.join(installation_path, "gfpgan"),
+            path.join(working_directory, "gfpgan"),
+            dirs_exist_ok=True,
+        )
+    if path.exists(path.join(installation_path, "last_detected_face.pkl")):
+        print(f"Copying last detected face from {installation_path}")
+        shutil.copyfile(
+            path.join(installation_path, "last_detected_face.pkl"),
+            path.join(working_directory, "last_detected_face.pkl"),
+        )
+    if path.exists(path.join(installation_path, "database.json")):
+        print(f"Copying database from {installation_path}")
+        shutil.copyfile(
+            path.join(installation_path, "database.json"),
+            path.join(working_directory, "database.json"),
+        )
+        database = load_database()
+        print("Updating output paths")
+        for run_id in database:
+            params = database[run_id]["params"]
+            output = params["output"]
+            if path.exists(output) and path.dirname(output) == installation_path:
+                params["output"] = path.join(working_directory, path.basename(output))
+        dump_database(database)
+
+
 # create essential directories
-os.makedirs(os.path.join(working_directory, "checkpoints"), exist_ok=True)
-os.makedirs(os.path.join(working_directory, "temp"), exist_ok=True)
-os.makedirs(os.path.join(working_directory, "face_alignment"), exist_ok=True)
-os.makedirs(os.path.join(working_directory, "results"), exist_ok=True)
+os.makedirs(path.join(working_directory, "checkpoints"), exist_ok=True)
+os.makedirs(path.join(working_directory, "temp"), exist_ok=True)
+os.makedirs(path.join(working_directory, "face_alignment"), exist_ok=True)
+os.makedirs(path.join(working_directory, "results"), exist_ok=True)
 
 # download mobilenet model
-print("downloading mobilenet essentials")
-load_file_from_url(
-    url="https://github.com/anothermartz/Easy-Wav2Lip/raw/v8.3/checkpoints/mobilenet.pth",
-    model_dir="checkpoints",
-    progress=True,
-    file_name="mobilenet.pth",
-)
+if not path.exists(path.join(working_directory, "checkpoints", "mobilenet.pth")):
+    print("downloading mobilenet essentials")
+    load_file_from_url(
+        url="https://github.com/anothermartz/Easy-Wav2Lip/raw/v8.3/checkpoints/mobilenet.pth",
+        model_dir="checkpoints",
+        progress=True,
+        file_name="mobilenet.pth",
+    )
 
 # download and initialize both wav2lip models
-print("downloading wav2lip essentials")
-load_file_from_url(
-    url="https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/Wav2Lip_GAN.pth",
-    model_dir="checkpoints",
-    progress=True,
-    file_name="Wav2Lip_GAN.pth",
-)
-model = load_model(os.path.join(working_directory, "checkpoints", "Wav2Lip_GAN.pth"))
-print("wav2lip_gan loaded")
-load_file_from_url(
-    url="https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/Wav2Lip.pth",
-    model_dir="checkpoints",
-    progress=True,
-    file_name="Wav2Lip.pth",
-)
-model = load_model(os.path.join(working_directory, "checkpoints", "Wav2Lip.pth"))
-print("wav2lip loaded")
+if not path.exists(path.join(working_directory, "checkpoints", "Wav2Lip_GAN.pk1")):
+    print("downloading wav2lip_gan essentials")
+    load_file_from_url(
+        url="https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/Wav2Lip_GAN.pth",
+        model_dir="checkpoints",
+        progress=True,
+        file_name="Wav2Lip_GAN.pth",
+    )
+    load_model(path.join(working_directory, "checkpoints", "Wav2Lip_GAN.pth"))
+    print("wav2lip_gan loaded")
+
+if not path.exists(path.join(working_directory, "checkpoints", "Wav2Lip.pk1")):
+    print("downloading wav2lip essentials")
+    load_file_from_url(
+        url="https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/Wav2Lip.pth",
+        model_dir="checkpoints",
+        progress=True,
+        file_name="Wav2Lip.pth",
+    )
+    load_model(path.join(working_directory, "checkpoints", "Wav2Lip.pth"))
+    print("wav2lip loaded")
 
 # download gfpgan files
-print("downloading gfpgan essentials")
-load_file_from_url(
-    url="https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/GFPGANv1.4.pth",
-    model_dir="checkpoints",
-    progress=True,
-    file_name="GFPGANv1.4.pth",
-)
+if not path.exists(path.join(working_directory, "checkpoints", "GFPGANv1.4.pth")):
+    print("downloading gfpgan essentials")
+    load_file_from_url(
+        url="https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/GFPGANv1.4.pth",
+        model_dir="checkpoints",
+        progress=True,
+        file_name="GFPGANv1.4.pth",
+    )
 load_sr()
 
 # load face detectors
-print("initializing face detectors")
-load_file_from_url(
-    url="https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/shape_predictor_68_face_landmarks_GTX.dat",
-    model_dir="checkpoints",
-    progress=True,
-    file_name="shape_predictor_68_face_landmarks_GTX.dat",
-)
-
+if not path.exists(
+    path.join(
+        working_directory, "checkpoints", "shape_predictor_68_face_landmarks_GTX.dat"
+    )
+):
+    print("initializing face detectors")
+    load_file_from_url(
+        url="https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/shape_predictor_68_face_landmarks_GTX.dat",
+        model_dir="checkpoints",
+        progress=True,
+        file_name="shape_predictor_68_face_landmarks_GTX.dat",
+    )
 load_predictor()
 
 # write a file to signify setup is done
 with open("installed.txt", "w") as f:
     f.write(version)
 print("Installation complete!")
-print(
-    "If you just updated from v8 - make sure to download the updated Easy-Wav2Lip.bat too!"
-)
